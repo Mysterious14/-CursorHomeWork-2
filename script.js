93c8402f4b8f001a523fafd6e79ccdbfe18003d4
Юрій Ірза <irzaurij04@gmail.com>
2022-08-21 17:03:16 +0300

    Add

---
+++ script.js
@@ -0,0 +1,19 @@
+let NumberN;
+while(!Number.isInteger(NumberN)){
+    NumberN = +prompt("Enter your N");
+}
+let NumberM;
+while(!Number.isInteger(NumberM)){
+    NumberM = +prompt("Enter your M");
+}
+const CheckParity = confirm("Skip even numbers?")
+let result=0;
+for(let i=NumberN; i<=NumberM; i++){
+    if(CheckParity){
+        !(i % 2) ? result:result+=i;
+        
+    }
+    else result+=i;
+
+}
+console.log(`Your result = ${result}`);
