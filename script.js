d0593c712f8f327af9c52183f138e9c1e61f11d3
Юрій Ірза <irzaurij04@gmail.com>
2022-08-22 00:25:56 +0300

    changes

--- script.js
+++ script.js
@@ -1,10 +1,25 @@
 let NumberN;
 while(!Number.isInteger(NumberN)){
-    NumberN = +prompt("Enter your N");
+    NumberN = +prompt("Enter your N (integer)");
+    if(!Number.isInteger(NumberN)){
+        alert("Enter correct values");
+    }
+    
+
+    
 }
 let NumberM;
 while(!Number.isInteger(NumberM)){
-    NumberM = +prompt("Enter your M");
+    
+    NumberM = +prompt("Enter your M (integer)");
+    
+    if(!Number.isInteger(NumberM)){
+        alert("Enter correct values");
+        continue;
+    }
+    while(NumberN>=NumberM){
+        NumberM = +prompt("I need M>N. Enter your M");
+    }
 }
 const CheckParity = confirm("Skip even numbers?")
 let result=0;
@@ -16,5 +31,5 @@ for(let i=NumberN; i<=NumberM; i++){
     else result+=i;
 
 }
-alert(`Your result = ${result}`)
-console.log(`Your result = ${result}`);
+alert(`Your N = ${NumberN}. Your M = ${NumberM}. Skip even numbers? ${CheckParity}. Your result = ${result}`);
+console.log(`Your N = ${NumberN}. Your M = ${NumberM}. Skip even numbers? ${CheckParity}. Your result = ${result}`);
